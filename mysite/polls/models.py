d24bbe388c0c69a3ddaeaeee3c4d2e660dca4387
ihsdefinitivo wido <huidobroinigo@gmail.com>
2022-11-30 23:38:04 +0000

    Project Completed

---
+++ mysite/polls/models.py
@@ -0,0 +1,24 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+#we’ll create two models: Question and Choice. A Question has a question and a publication date
+#Here, each model is represented by a class that subclasses django.db.models.Model.
+#Each model has a number of class variables, each of which represents a database field in the model.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
